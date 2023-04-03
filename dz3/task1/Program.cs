9f5e617bb6cf1980eadf0a13abe1ef4d49c19acd
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-03 17:49:15 +0300

    added task1

---
+++ dz3/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message}");
+    return System.Convert.ToInt32(Console.ReadLine());
+}
+
+string Palindrome(int number)
+{
+    int rightSide = number % 100;
+    int leftSide = number / 1000;
+    if (number / 10000 == rightSide % 10 && rightSide / 10 == leftSide % 10)
+    {
+        System.Console.Write("Является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Не является палиндромом");
+    }
+    return "";
+}
+
+int number = ReadInt("Введите число: ");
+bool CountNumbers(int count)
+{
+    if (number < 9999 || number > 99999)
+    {
+        System.Console.WriteLine("Число не пятизначное");
+        return false;
+    }
+    return true;
+}
+
+if (CountNumbers(number))
+{
+    string count = Palindrome(number);
+    System.Console.WriteLine(count);
+}
