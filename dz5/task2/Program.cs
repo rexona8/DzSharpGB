4ee7c9ad4440fd31fe7e1a9946944deec41d6b79
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-14 17:34:25 +0300

    added task2 decision

--- dz5/task2/Program.cs
+++ dz5/task2/Program.cs
@@ -1,3 +1,48 @@
 ﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму 
 // элементов, стоящих на нечётных позициях.
 
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg}");
+    return System.Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+void EvenNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            sum = sum + arr[i];
+        }
+    }
+    System.Console.WriteLine($"Сумма нечётных чисел в массиве равна: {sum}");
+}
+
+int len = ReadInt("Введите число: ");
+int min = ReadInt("Введите минимальную границу: ");
+int max = ReadInt("Введите максимальную границу: ");
+int[] array = CreateArray(len, min, max);
+PrintArray(array);
+EvenNumbers(array);
