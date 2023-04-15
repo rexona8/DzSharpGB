9fcacf625ecbbcea15173b6eff77220e8945cd6d
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-15 21:41:57 +0300

    added task2 decision

--- dz6/task2/Program.cs
+++ dz6/task2/Program.cs
@@ -2,3 +2,20 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
 // задаются пользователем.
 
+System.Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+ 
+ 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+System.Console.WriteLine($"Пересечение точек: {x}; {y}");
