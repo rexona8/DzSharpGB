7bf529240d9ccb647a20ff3a90b7b8d0b458874e
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-27 21:01:36 +0300

    added decision for task2

--- dz9/task2/Program.cs
+++ dz9/task2/Program.cs
@@ -3,3 +3,13 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
+int SumEvenNum(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    return m + SumEvenNum(m + 1, n);
+}
+
+System.Console.Write(SumEvenNum(1, 15));
