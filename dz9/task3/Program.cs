c578f3f4af225b0cc79f5b0909bc068e6c624ff1
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-27 21:40:02 +0300

    added decision for task3

--- dz9/task3/Program.cs
+++ dz9/task3/Program.cs
@@ -2,3 +2,11 @@
 // Даны два неотрицательных числа m и n. 
 // m = 3, n = 2 -> A(m,n) = 29
 
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана: m = 3, n = 2 -> A(m,n) = {akkerman(3, 2)}");
