83722ffe89a628a05c55e4c2c9f22b603286fc61
Nikita Vasilchenko <rexona8@yandex.ru>
2023-04-27 21:52:15 +0300

    added dz9

---
+++ dz9/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n. 
+// m = 3, n = 2 -> A(m,n) = 29
+
+int akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return akkerman(m - 1, 1);
+    }
+    else
+    {
+        return akkerman(m - 1, akkerman(m, n - 1));
+    }
+}
+
+Console.Write($"Функция Аккермана: m = 3, n = 2 -> A(m,n) = {akkerman(3, 2)}");
